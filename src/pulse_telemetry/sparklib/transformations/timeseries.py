60ad167a3da25cbdc3db0eae5b051257c01e6e16
Erik Amundson <erik0422@gmail.com>
2024-09-25 14:57:06 -0700

    fix pyproject, materialize date column for partitioning

--- src/pulse_telemetry/sparklib/transformations/timeseries.py
+++ src/pulse_telemetry/sparklib/transformations/timeseries.py
@@ -9,6 +9,7 @@ timeseries_schema = T.StructType(
         T.StructField("step_id", dataType=T.IntegerType(), nullable=True),
         T.StructField("cycle_number", dataType=T.IntegerType(), nullable=False),
         T.StructField("timestamp", dataType=T.TimestampType(), nullable=False),
+        T.StructField("date", dataType=T.DateType(), nullable=False),
         T.StructField("current__A", dataType=T.DoubleType(), nullable=False),
         T.StructField("voltage__V", dataType=T.DoubleType(), nullable=False),
         T.StructField("power__W", dataType=T.DoubleType(), nullable=False),
