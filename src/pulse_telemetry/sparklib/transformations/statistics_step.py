f3090bf086cd4957361ab777507d8b52c82d54a9
Maxwell Dylla <maxwell.dylla@gmail.com>
2024-10-01 13:56:44 -0700

    add comments to schema and transform

--- src/pulse_telemetry/sparklib/transformations/statistics_step.py
+++ src/pulse_telemetry/sparklib/transformations/statistics_step.py
@@ -1,6 +1,10 @@
+from typing import TYPE_CHECKING
+
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
-from pyspark.sql import DataFrame
+
+if TYPE_CHECKING:
+    from pyspark.sql import DataFrame
 
 
 statistics_step_schema = T.StructType(
@@ -12,11 +16,9 @@ statistics_step_schema = T.StructType(
         T.StructField("step_number", dataType=T.LongType(), nullable=False),
         T.StructField("step_type", dataType=T.StringType(), nullable=True),
         T.StructField("step_id", dataType=T.IntegerType(), nullable=True),
-
         # Time
         T.StructField("start_time", dataType=T.TimestampType(), nullable=False),
         T.StructField("end_time", dataType=T.TimestampType(), nullable=False),
-
         # Current
         T.StructField("start_current__A", dataType=T.DoubleType(), nullable=False),
         T.StructField("end_current__A", dataType=T.DoubleType(), nullable=False),
@@ -25,14 +27,12 @@ statistics_step_schema = T.StructType(
         T.StructField("max_current__A", dataType=T.DoubleType(), nullable=False),
         T.StructField("max_abs_current__A", dataType=T.DoubleType(), nullable=False),
         T.StructField("mean_current__A", dataType=T.DoubleType(), nullable=False),
-
         # Voltage
         T.StructField("start_voltage__V", dataType=T.DoubleType(), nullable=False),
         T.StructField("end_voltage__V", dataType=T.DoubleType(), nullable=False),
         T.StructField("min_voltage__V", dataType=T.DoubleType(), nullable=False),
         T.StructField("max_voltage__V", dataType=T.DoubleType(), nullable=False),
         T.StructField("mean_voltage__V", dataType=T.DoubleType(), nullable=False),
-
         # Power
         T.StructField("start_power__W", dataType=T.DoubleType(), nullable=False),
         T.StructField("end_power__W", dataType=T.DoubleType(), nullable=False),
@@ -41,20 +41,18 @@ statistics_step_schema = T.StructType(
         T.StructField("max_power__W", dataType=T.DoubleType(), nullable=False),
         T.StructField("max_abs_power__W", dataType=T.DoubleType(), nullable=False),
         T.StructField("mean_power__W", dataType=T.DoubleType(), nullable=False),
-
         # Accumulations (within the step)
         T.StructField("charge_capacity__Ah", dataType=T.DoubleType(), nullable=False),
         T.StructField("discharge_capacity__Ah", dataType=T.DoubleType(), nullable=False),
         T.StructField("charge_energy__Wh", dataType=T.DoubleType(), nullable=False),
         T.StructField("discharge_energy__Wh", dataType=T.DoubleType(), nullable=False),
-
         # Metadata
         T.StructField("update_ts", dataType=T.TimestampType(), nullable=False),
     ]
 )
 
 
-def statistics_step(df: DataFrame):
+def statistics_step(df: "DataFrame") -> "DataFrame":
     """Returns step-level statistics of the timeseries data.
 
     Parameters
@@ -70,7 +68,8 @@ def statistics_step(df: DataFrame):
     Notes
     -----
     Can be applied in both batch and streaming contexts, but it is reccomended to always run a
-    batch job to finalize late data. In streaming mode, use a watermark to limit in-memory state:
+    batch job to finalize statistics for late data. In streaming mode, use a watermark to limit
+    in-memory state. Use update_ts to avoid issues with backfill of historical data:
 
     ```
     df = df.withWatermark("update_ts", "14 days")
@@ -85,11 +84,9 @@ def statistics_step(df: DataFrame):
         # Identifiers (groupbys are already included)
         F.first("step_type", ignorenulls=True).alias("step_type"),  # Will return null if all null
         F.first("step_id", ignorenulls=True).alias("step_id"),
-
         # Time
         F.min("timestamp").alias("start_time"),
         F.max("timestamp").alias("end_time"),
-
         # Current
         F.min_by("current__A", "timestamp").alias("start_current__A"),
         F.max_by("current__A", "timestamp").alias("end_current__A"),
@@ -98,14 +95,12 @@ def statistics_step(df: DataFrame):
         F.max("current__A").alias("max_current__A"),
         F.max(F.abs("current__A")).alias("max_abs_current__A"),
         F.avg("current__A").alias("mean_current__A"),
-
         # Voltage
         F.min_by("voltage__V", "timestamp").alias("start_voltage__V"),
         F.max_by("voltage__V", "timestamp").alias("end_voltage__V"),
         F.min("voltage__V").alias("min_voltage__V"),
         F.max("voltage__V").alias("max_voltage__V"),
         F.avg("voltage__V").alias("mean_voltage__V"),
-
         # Power
         F.min_by("power__W", "timestamp").alias("start_power__W"),
         F.max_by("power__W", "timestamp").alias("end_power__W"),
@@ -114,13 +109,11 @@ def statistics_step(df: DataFrame):
         F.max("power__W").alias("max_power__W"),
         F.max(F.abs("power__W")).alias("max_abs_power__W"),
         F.avg("power__W").alias("mean_power__W"),
-
-        # Accumulations (within the step)
-        F.sum(F.when(F.col("current__A") >= 0, F.col("capacity__Ah")).otherwise(0.0)).alias("charge_capacity__Ah"),
-        F.sum(F.when(F.col("current__A") < 0, F.col("capacity__Ah")).otherwise(0.0)).alias("discharge_capacity__Ah"),
-        F.sum(F.when(F.col("current__A") >= 0, F.col("energy__Wh")).otherwise(0.0)).alias("charge_energy__Wh"),
-        F.sum(F.when(F.col("current__A") < 0, F.col("energy__Wh")).otherwise(0.0)).alias("discharge_energy__Wh"),
-
+        # Accumulations (within the step) - charging is negative current
+        F.sum(F.when(F.col("current__A") <= 0, F.col("capacity__Ah")).otherwise(0.0)).alias("charge_capacity__Ah"),
+        F.sum(F.when(F.col("current__A") > 0, F.col("capacity__Ah")).otherwise(0.0)).alias("discharge_capacity__Ah"),
+        F.sum(F.when(F.col("current__A") <= 0, F.col("energy__Wh")).otherwise(0.0)).alias("charge_energy__Wh"),
+        F.sum(F.when(F.col("current__A") > 0, F.col("energy__Wh")).otherwise(0.0)).alias("discharge_energy__Wh"),
         # Metadata
         F.current_timestamp().alias("update_ts"),  # Timestamp in timezone configured in Spark environment
     )
--- src/pulse_telemetry/sparklib/transformations/timeseries.py
+++ src/pulse_telemetry/sparklib/transformations/timeseries.py
@@ -4,23 +4,33 @@ timeseries_schema = T.StructType(
     [
         # Identifiers
         T.StructField("device_id", dataType=T.StringType(), nullable=False),  # ID of the measuring device
-        T.StructField("test_id", dataType=T.StringType(), nullable=False),  # ID of the sequence (i.e. test) being measured by the device
+        T.StructField(
+            "test_id", dataType=T.StringType(), nullable=False
+        ),  # ID of the sequence (i.e. test) being measured by the device
         T.StructField("cycle_number", dataType=T.IntegerType(), nullable=False),  # Cycle number within the sequence
         T.StructField("step_number", dataType=T.LongType(), nullable=False),  # Step number within the sequence
         T.StructField("step_type", dataType=T.StringType(), nullable=True),  # Step type as a human-readable string
         T.StructField("step_id", dataType=T.IntegerType(), nullable=True),  # Unique ID for the step type
         T.StructField("record_number", dataType=T.LongType(), nullable=False),  # Record number within the sequence
         # Data
-        T.StructField("timestamp", dataType=T.TimestampType(), nullable=False),  # Timestamp when the record was recorded
+        T.StructField(
+            "timestamp", dataType=T.TimestampType(), nullable=False
+        ),  # Timestamp when the record was recorded
         T.StructField("date", dataType=T.DateType(), nullable=False),  # Date when the data was recorded, for partions
         T.StructField("current__A", dataType=T.DoubleType(), nullable=False),  # Instantanious current
         T.StructField("voltage__V", dataType=T.DoubleType(), nullable=False),  # Instantanious voltage
         T.StructField("power__W", dataType=T.DoubleType(), nullable=False),  # Instantanious power
-        T.StructField("capacity__Ah", dataType=T.DoubleType(), nullable=False),  # Capacity accumulated during this record
+        T.StructField(
+            "capacity__Ah", dataType=T.DoubleType(), nullable=False
+        ),  # Capacity accumulated during this record
         T.StructField("energy__Wh", dataType=T.DoubleType(), nullable=False),  # Energy accumulated during this record
-        T.StructField("auxiliary", dataType=T.MapType(T.StringType(), T.DoubleType()), nullable=True),  # Auxiliary measurements (e.g. temperature)
+        T.StructField(
+            "auxiliary", dataType=T.MapType(T.StringType(), T.DoubleType()), nullable=True
+        ),  # Auxiliary measurements (e.g. temperature)
         T.StructField("metadata", dataType=T.StringType(), nullable=True),  # JSON string for user-specified fields
         # Metadata
-        T.StructField("update_ts", dataType=T.TimestampType(), nullable=False),  # Timestamp when this data was processed by the pulse application
+        T.StructField(
+            "update_ts", dataType=T.TimestampType(), nullable=False
+        ),  # Timestamp when this data was processed by the pulse application
     ]
 )
