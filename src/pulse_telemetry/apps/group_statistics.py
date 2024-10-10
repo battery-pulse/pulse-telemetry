3986048aea9439d3164d643f2fcff7a03a09ffc3
Maxwell Dylla <maxwell.dylla@gmail.com>
2024-10-10 15:00:28 -0700

    comments on partitions

--- src/pulse_telemetry/apps/group_statistics.py
+++ src/pulse_telemetry/apps/group_statistics.py
@@ -1,8 +1,8 @@
 telemetry_partitions = "PARTITIONED BY device_id, test_id, month(timestamp);"
 telemetry_writes = "WRITE DISTRIBUTED BY PARTITION LOCALLY ORDERED BY cycle_number, step_number, record_number"
 
-statistics_step_partitions = "PARTITIONED BY year(start_time);"
+statistics_step_partitions = "PARTITIONED BY year(start_time);"  # Only if you have 1000 channels
 statistics_step_writes = "WRITE ORDERED BY test_id, cycle_number, step_number"
 
-statistics_cycle_partitions = "PARTITIONED BY year(start_time);"
+statistics_cycle_partitions = "PARTITIONED BY year(start_time);"  # Only if you have 1000 channels
 statistics_cycle_writes = "WRITE ORDERED BY test_id, cycle_number"
