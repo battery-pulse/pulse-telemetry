17afa58f24b919c038887ee493b3fbf8390dedee
Maxwell Dylla <maxwell.dylla@gmail.com>
2024-10-10 12:06:35 -0700

    note on partitions

--- src/pulse_telemetry/apps/group_statistics.py
+++ src/pulse_telemetry/apps/group_statistics.py
@@ -1,3 +1,3 @@
 partitions_telemetry = ["device-id", "test-id", "month"]
-partitions_statistics_step = ["year"]
-partitions_statistics_cycle = ["year"]
+partitions_statistics_step = ["year"]  # only partition if large number of test channels
+partitions_statistics_cycle = ["year"]  # only partition if large number of test channels
